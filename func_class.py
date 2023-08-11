225adee94e5dfa5bbb811872adb990165eed0887
danya_violet <66GsHHDDZsgJeU4a29qC>
2023-08-11 20:35:31 +0300

    Player.draw

--- func_class.py
+++ func_class.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from typing import Any
+
+from pygame import draw
+
+from const import red, cell_size
 
 
 class Direction(Enum):
@@ -12,3 +17,6 @@ class Player:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    def draw(self, screen: Any):
+        draw.rect(screen, red, (self.x * cell_size, self.y * cell_size, cell_size, cell_size))
