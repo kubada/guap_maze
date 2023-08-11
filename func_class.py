3c7fd3fb65de51574f0567f2f02f437db4dac70f
danya_violet <66GsHHDDZsgJeU4a29qC>
2023-08-11 20:11:43 +0300

    Direction, Player.init

---
+++ func_class.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = (0, -1)
+    DOWN = (0, 1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+
+
+class Player:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
